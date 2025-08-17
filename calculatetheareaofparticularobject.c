8a89e6ea276fbd8194108d0342ab2ac3abc40452
Nirmal <nirmalbhandari0200@gmail.com>
2025-08-17 10:17:33 +0530

    Add files via upload

---
+++ calculatetheareaofparticularobject.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice;
+    float R, L, B, side;
+
+    printf("\n**** MENU ****");
+    printf("\n1. Area of the Circle");
+    printf("\n2. Area of the Rectangle");
+    printf("\n3. Area of the Square");
+    printf("\n4. Area of the Triangle");
+    printf("\n5. Exit");
+
+    printf("\n\nEnter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("Enter radius of circle: ");
+        scanf("%f", &R);
+        printf("Area of Circle = %f",3.14*R*R);
+        break;
+
+    case 2:
+        printf("Enter length and breadth of rectangle: ");
+        scanf("%f %f", &L, &B);
+        printf("Area of Rectangle = %f",L*B);
+        break;
+
+    case 3:
+        printf("Enter side of square: ");
+        scanf("%f", &side);
+        printf("Area of Square =%f",side*side);
+        break;
+
+    default :
+        printf("Invalid Input");
+
+    }
+    return 0;
+}
+
+
